fd5b74be9771d8191d9d4bab36cfb66066a187ef
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2024-10-07 21:19:35 +0530

    prettier

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
 module.exports = withNextra({
-i18n: {
-  locales: ['en', 'zh-CN'],
-  defaultLocale: 'en'
-}
-})
+  i18n: {
+    locales: ["en", "zh-CN"],
+    defaultLocale: "en",
+  },
+});
